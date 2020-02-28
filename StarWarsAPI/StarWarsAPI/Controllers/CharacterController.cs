f034fd432d42a7dce575292df34f7a7135fd0713
KamilBracki <43040670+KamilBracki@users.noreply.github.com>
2020-02-28 14:35:05 +0100

    Add getCharacters with hrdcoded list

--- StarWarsAPI/StarWarsAPI/Controllers/CharacterController.cs
+++ StarWarsAPI/StarWarsAPI/Controllers/CharacterController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StarWarsAPI.Models;
 
 namespace StarWarsAPI.Controllers
 {
@@ -11,5 +12,15 @@ namespace StarWarsAPI.Controllers
     [ApiController]
     public class CharacterController : ControllerBase
     {
+
+        [HttpGet]
+        public List<Character> GetCharacters()
+        {
+            List<Character> characters = new List<Character>();
+            characters.Add(new Character { Name = "John", CharacterId = 1, Episodes = new List<string>(){ "aaa", "ww" }, Friends = new List<string>() { "AS", "zX" } } );
+            characters.Add(new Character { Name = "John2", CharacterId = 1, Episodes = new List<string>() { "aaa2", "ww2" }, Friends = new List<string>() { "AS2", "zX2" } });
+
+            return characters;
+        }
     }
 }
