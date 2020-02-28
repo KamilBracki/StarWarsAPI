b7423c6bb320d02a9b22d64e3c2e066a7fc8b2f1
KamilBracki <43040670+KamilBracki@users.noreply.github.com>
2020-02-28 14:18:55 +0100

    Add empty Charcter Controller

---
+++ StarWarsAPI/StarWarsAPI/Controllers/CharacterController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StarWarsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+    }
+}
