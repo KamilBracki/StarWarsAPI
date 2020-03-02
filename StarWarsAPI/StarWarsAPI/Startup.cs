a67eafdad4ea87d0b0980046f90f6078b38f0c1d
KamilBracki <43040670+KamilBracki@users.noreply.github.com>
2020-03-02 14:36:38 +0100

    Add swagger

--- StarWarsAPI/StarWarsAPI/Startup.cs
+++ StarWarsAPI/StarWarsAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using StarWarsAPI.AccessLayer;
 
 namespace StarWarsAPI
@@ -31,6 +32,11 @@ namespace StarWarsAPI
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddDbContext<StarContext>(context => context
                 .UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "StarWars API", Version = "v1" });
+            });
         }
 
         
@@ -42,6 +48,12 @@ namespace StarWarsAPI
 
                 context.Database.EnsureCreated();
             }
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarWars API V1");
+            });
 
             if (env.IsDevelopment())
             {
